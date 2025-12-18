0287d43550fb641452105e4e321ba4716f6b0d4f
devch9 <devch819@gmail.com>
2025-12-18 16:05:02 +0530

    14-12-2025 Added filter options to Service Requests page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
 import { GlobalClickDirective } from '../../utilities/directives/global-click.directive';
@@ -6,6 +12,7 @@ import { StorageService } from '../../utilities/services/storage.service';
 import { Subject, takeUntil } from 'rxjs';
 import { FormsModule } from '@angular/forms';
 import { SearchPipe } from '../../utilities/pipes/search.pipe';
+import { CommonModule } from '@angular/common';
 
 type MenuTab = 'requests' | 'faqs' | 'terms' | 'contact';
 @Component({
@@ -15,7 +22,8 @@ type MenuTab = 'requests' | 'faqs' | 'terms' | 'contact';
     MatMenuModule,
     GlobalClickDirective,
     FormsModule,
-    SearchPipe
+    SearchPipe,
+    CommonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
@@ -23,48 +31,33 @@ type MenuTab = 'requests' | 'faqs' | 'terms' | 'contact';
 })
 export class HeaderComponent {
   private destroy$ = new Subject<void>();
-  activeSupportMenu: boolean = false;
 
-  constructor(private router: Router, private storage_service: StorageService) {
+  constructor(
+    private router: Router,
+    private storage_service: StorageService
+  ) {
     this.router.events.subscribe(() => {
       this.activeSupportMenu = Object.values(this.menuRoutes).includes(this.router.url);
     });
   }
 
-  menuRoutes: Record<MenuTab, string> = {
-    requests: '/dashboard/service-requests',
-    faqs: '/dashboard/faqs',
-    terms: '/dashboard/terms',
-    contact: '/dashboard/contact',
-  };
-  activeMenuTab: Record<MenuTab, boolean> = {
-    requests: false,
-    faqs: false,
-    terms: false,
-    contact: false,
-  };
-
-  setActive() {
-    this.activeSupportMenu = true;
-  }
-
-  setActiveMenuTab(tab: string) {
-  this.activeMenuTab = {
-    requests: false,
-    faqs: false,
-    terms: false,
-    contact: false,
-  };
-  (this.activeMenuTab as any)[tab] = true;
-  this.router.events.subscribe(() => {
-    (this.activeMenuTab as any)[tab] = Object.values(this.menuRoutes).includes(this.router.url)
-
-  });
-}
+  activeSupportMenu: boolean = false;
 
   searchSite!: string;
   sitesList!: Array<any>;
   ngOnInit() {
+    this.storage_service.siteData$.pipe(takeUntil(this.destroy$)).subscribe({
+      next: (res: any) => {
+        if (res.length === 0) return;
+        this.sitesList = res.sites;
+        this.currentSite = this.sitesList[0];
+        const current = this.storage_service.currentSite$.getValue();
+        if (!current) {
+          this.storage_service.currentSite$.next(this.currentSite);
+        }
+      },
+    });
+
     this.storage_service.siteData$.pipe(takeUntil(this.destroy$)).subscribe({
       next: (res: any) => {
         if (res.length === 0) return;
@@ -93,6 +86,38 @@ export class HeaderComponent {
     });
   }
 
+  menuRoutes: Record<MenuTab, string> = {
+    requests: '/dashboard/service-requests',
+    faqs: '/dashboard/faqs',
+    terms: '/dashboard/terms',
+    contact: '/dashboard/contact',
+  };
+  activeMenuTab: Record<MenuTab, boolean> = {
+    requests: false,
+    faqs: false,
+    terms: false,
+    contact: false,
+  };
+
+  setActive() {
+    this.activeSupportMenu = true;
+  }
+
+  setActiveMenuTab(tab: string) {
+    this.activeMenuTab = {
+      requests: false,
+      faqs: false,
+      terms: false,
+      contact: false,
+    };
+    (this.activeMenuTab as any)[tab] = true;
+    this.router.events.subscribe(() => {
+      (this.activeMenuTab as any)[tab] = Object.values(
+        this.menuRoutes
+      ).includes(this.router.url);
+    });
+  }
+
   currentSite: any;
   showSite: boolean = false;
   set(site: any) {
@@ -106,8 +131,8 @@ export class HeaderComponent {
     this.searchSite = '';
     this.showSite = !this.showSite;
     setTimeout(() => {
-      this.siteInput.nativeElement?.focus()
-    }, 500)
+      this.siteInput.nativeElement?.focus();
+    }, 500);
   }
 
   logout() {
