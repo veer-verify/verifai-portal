12069ed3e734c946621f533d5ff42e786ae855dd
devch9 <devch819@gmail.com>
2026-01-10 17:01:35 +0530

    trends-insights

---
+++ src/app/nav/insights/insights.component.ts
@@ -0,0 +1,155 @@
+import { Component } from '@angular/core';
+import { AgCharts } from 'ag-charts-angular';
+import {
+  AgBarSeriesOptions,
+  AgCategoryAxisOptions,
+  AgChartCaptionOptions,
+  AgChartLegendOptions,
+  AgChartOptions,
+  AgChartSubtitleOptions,
+  AgLineSeriesOptions,
+  AgNumberAxisOptions,
+  BarSeriesModule,
+  CategoryAxisModule,
+  LegendModule,
+  LineSeriesModule,
+  ModuleRegistry,
+  NumberAxisModule,
+  DonutSeriesModule,
+} from 'ag-charts-community';
+
+import {
+  CellClickedEvent,
+  ColDef,
+  GridOptions,
+  GridReadyEvent,
+  IServerSideDatasource,
+  themeQuartz,
+} from 'ag-grid-community';
+import { AgGridAngular } from 'ag-grid-angular';
+import { gridOptions } from '../../../grid.config';
+
+ModuleRegistry.registerModules([
+  BarSeriesModule,
+  CategoryAxisModule,
+  LegendModule,
+  LineSeriesModule,
+  NumberAxisModule,
+  DonutSeriesModule,
+]);
+
+@Component({
+  selector: 'app-insights',
+  imports: [AgCharts, AgGridAngular],
+  templateUrl: './insights.component.html',
+  styleUrl: './insights.component.css',
+})
+export class InsightsComponent {
+  public chartOptions!: AgChartOptions;
+
+  columnDefs: ColDef[] = [
+    { field: 'name', headerName: 'Col 1', filter: false },
+    { field: 'Col 2' , filter: false},
+    { field: 'eventFromTime', headerName: 'Col 3' , filter: false},
+    { field: 'eventToTime', headerName: 'Col 4' , filter: false},
+    { field: 'Col 5' , filter: false},
+    { field: 'objectName', headerName: 'Col 6' , filter: false},
+    { field: 'Col 7' , filter: false},
+    {
+      field: 'Col 8', filter: false,
+      cellRenderer: (params: any) => {
+        const isDisabled = params.data.files.length === 0;
+        const disabledAttr = isDisabled ? 'disabled' : '';
+        const style = isDisabled
+          ? 'style="opacity: 0.5; pointer-events: none; filter: grayscale(1);"'
+          : '';
+        return `<img src="icons/play-circle-fill.svg" class="btn-open" ${disabledAttr} ${style} />`;
+    },
+      editable: false,
+      sortable: false,
+    },
+  ];
+
+  rowData = [
+  {
+    name: 'Camera 01',
+    eventDate: '2026-01-09',
+    eventFromTime: '10:15 AM',
+    eventToTime: '10:18 AM',
+    duration: '00:03:00',
+    objectName: 'Person',
+    actionTag: 'Intrusion',
+    files: ['clip_01.mp4']
+  },
+  {
+    name: 'Camera 02',
+    eventDate: '2026-01-09',
+    eventFromTime: '11:02 AM',
+    eventToTime: '11:04 AM',
+    duration: '00:02:00',
+    objectName: 'Vehicle',
+    actionTag: 'Loitering',
+    files: [] // ❌ play icon disabled
+  },
+  {
+    name: 'Camera 03',
+    eventDate: '2026-01-09',
+    eventFromTime: '12:45 PM',
+    eventToTime: '12:50 PM',
+    duration: '00:05:00',
+    objectName: 'Person',
+    actionTag: 'Trespassing',
+    files: ['clip_03.mp4']
+  },
+  {
+    name: 'Camera 04',
+    eventDate: '2026-01-09',
+    eventFromTime: '02:10 PM',
+    eventToTime: '02:12 PM',
+    duration: '00:02:00',
+    objectName: 'Animal',
+    actionTag: 'Motion Detected',
+    files: [] // ❌ play icon disabled
+  },
+  {
+    name: 'Camera 05',
+    eventDate: '2026-01-09',
+    eventFromTime: '03:30 PM',
+    eventToTime: '03:35 PM',
+    duration: '00:05:00',
+    objectName: 'Person',
+    actionTag: 'Running',
+    files: ['clip_05.mp4']
+  }
+];
+
+  gridOptions!: GridOptions
+
+  ngOnInit(){
+    this.gridOptions = gridOptions;
+  }
+
+  constructor() {
+    this.chartOptions = {
+      data: [
+        { day: 'Mon', users: 30 },
+        { day: 'Tue', users: 50 },
+        { day: 'Wed', users: 40 },
+        { day: 'thu', users: 32 },
+        { day: 'fri', users: 29 },
+        { day: 'sat', users: 37 },
+      ],
+      series: [
+        {
+          type: 'donut',
+          calloutLabelKey: 'day',
+          angleKey: 'users',
+          // innerRadiusRatio: 0.75, // thin ring
+          // cornerRadius: 20, // rounded ends
+          // sectorSpacing: 3
+          // padAngle: 3,
+        },
+      ],
+    };
+  }
+}
--- src/app/nav/trends/trends.component.ts
+++ src/app/nav/trends/trends.component.ts
@@ -11,7 +11,7 @@ import { MatOptionModule } from '@angular/material/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
-import { AgCharts } from "ag-charts-angular";
+import { AgCharts } from 'ag-charts-angular';
 import {
   AgBarSeriesOptions,
   AgCategoryAxisOptions,
@@ -27,8 +27,9 @@ import {
   LineSeriesModule,
   ModuleRegistry,
   NumberAxisModule,
-} from "ag-charts-community";
+} from 'ag-charts-community';
 import { filter } from 'rxjs';
+import { gridOptions } from '../../../grid.config';
 
 ModuleRegistry.registerModules([
   BarSeriesModule,
@@ -49,7 +50,7 @@ ModuleRegistry.registerModules([
     ReactiveFormsModule,
     MatSelectModule,
     MatInputModule,
-    AgCharts
+    AgCharts,
   ],
   templateUrl: './trends.component.html',
   styleUrl: './trends.component.css',
@@ -57,77 +58,36 @@ ModuleRegistry.registerModules([
 export class TrendsComponent {
   public chartOptions!: AgChartOptions;
 
-
   constructor(
     private storage_service: StorageService,
     private insight_service: InsightService,
     private fb: FormBuilder
   ) {
     this.chartOptions = {
-      // Chart Title
-      title: { text: "Ice Cream Sales and Avg Temp" } as AgChartCaptionOptions,
-      // Chart Subtitle
-      subtitle: { text: "Data from 2022" } as AgChartSubtitleOptions,
-      // Data: Data to be displayed within the chart
       data: [
-        { month: "Jan", avgTemp: 2.3, iceCreamSales: 162000 },
-        { month: "Mar", avgTemp: 6.3, iceCreamSales: 302000 },
-        { month: "May", avgTemp: 16.2, iceCreamSales: 800000 },
-        { month: "Jul", avgTemp: 22.8, iceCreamSales: 1254000 },
-        { month: "Sep", avgTemp: 14.5, iceCreamSales: 950000 },
-        { month: "Nov", avgTemp: 8.9, iceCreamSales: 200000 },
+        { day: 'Mon', users: 30 },
+        { day: 'Tue', users: 50 },
+        { day: 'Wed', users: 40 },
+        { day: 'thu', users: 32 },
+        { day: 'fri', users: 29 },
+        { day: 'sat', users: 37 },
       ],
-      // Series: Defines which chart type and data to use
       series: [
         {
-          type: "bar",
-          xKey: "month",
-          yKey: "iceCreamSales",
-          yName: "Ice Cream Sales",
-          // Optional Y Axis Key, to link series to an axis, with better code readability
-          yKeyAxis: "priceAxis",
-        } as AgBarSeriesOptions,
-        {
-          type: "line",
-          xKey: "month",
-          yKey: "avgTemp",
-          yName: "Average Temperature (°C)",
-          // Optional Y Axis Key, to link series to an axis, with better code readability
-          yKeyAxis: "temperatureAxis",
-        } as AgLineSeriesOptions,
+          type: 'line',
+          xKey: 'day',
+          yKey: 'users',
+          stroke: '#ed3237',
+          strokeWidth: 3,
+          marker: {
+            enabled: true,
+            fill: '#ed3237', // dot border
+            size: 8,
+          },
+        },
       ],
-      // Axes: Configure the axes for the chart
-      axes: {
-        // Use left axis for 'iceCreamSales' series, referencing the yKeyAxis value
-        priceAxis: {
-          type: "number",
-          position: "left",
-          // Format the label applied to this axis
-          label: {
-            formatter: (params) => {
-              return parseFloat(params.value).toLocaleString();
-            },
-          },
-        } as AgNumberAxisOptions,
-        // Use right axis for 'avgTemp' series, referencing the yKeyAxis value
-        temperatureAxis: {
-          type: "number",
-          position: "right",
-          // Format the label applied to this axis (append ' °C')
-          label: {
-            formatter: (params) => {
-              return params.value + " °C";
-            },
-          },
-        } as AgNumberAxisOptions,
-      },
-      // Legend: Matches visual elements to their corresponding series or data categories.
-      legend: {
-        position: "right",
-      } as AgChartLegendOptions,
     };
   }
-
 
   /* 🔥 REQUIRED FOR HIGHCHARTS */
 
@@ -137,20 +97,21 @@ export class TrendsComponent {
   selectedDate: any = null;
   selectedId: any;
 
-
   ngOnInit() {
-    this.storage_service.currentSite$.pipe(filter((site) => !!site)).subscribe((res: any) => {
-      this.insight_service
-        .getNonWorkingDays(res.siteId)
-        .subscribe((dateRes: any) => {
-          if (dateRes.status === 'Success') {
-            this.disabledDates = dateRes.NotWorkingDaysList;
-            this.selectedDate = dateRes.LastWorkingDay;
+    this.storage_service.currentSite$
+      .pipe(filter((site) => !!site))
+      .subscribe((res: any) => {
+        this.insight_service
+          .getNonWorkingDays(res.siteId)
+          .subscribe((dateRes: any) => {
+            if (dateRes.status === 'Success') {
+              this.disabledDates = dateRes.NotWorkingDaysList;
+              this.selectedDate = dateRes.LastWorkingDay;
 
-            this.getServices();
-          }
-        });
-    });
+              this.getServices();
+            }
+          });
+      });
   }
 
   /* ✅ FIXED DATE FILTER */
@@ -177,7 +138,4 @@ export class TrendsComponent {
         });
     });
   }
-
-
-
 }
