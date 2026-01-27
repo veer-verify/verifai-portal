6d9d9e5eba604af9fdd4434de1c47ebc8f42dbf8
devch9 <devch819@gmail.com>
2026-01-27 12:06:33 +0530

    sitesInfo-2

--- src/app/nav/siteinfo/siteinfo.component.ts
+++ src/app/nav/siteinfo/siteinfo.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-siteinfo',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './siteinfo.component.html',
   styleUrl: './siteinfo.component.css'
 })
 export class SiteinfoComponent {
+
+  info = 1
+
+  det = [false, false, false];
+
+  changeClass(n: number){
+    this.info = n;
+  }
+
+  showMore(n: number){
+    this.det[n] = !this.det[n];
+  }
 
 }
