c0d3bb5f326a357b0f37e253b9c67650da690999
nallurisaisreedhar-4241 <nallurisaisreedhar@gmail.com>
2026-04-22 17:03:55 +0530

    feat: Add password validation rules and error handling in change password component

--- src/app/nav/profile/change-password/change-password.component.ts
+++ src/app/nav/profile/change-password/change-password.component.ts
@@ -21,36 +21,85 @@ export class ChangePasswordComponent {
   showNewPassword = signal(false);
   showConfirmPassword = signal(false);
 
-  changePasswordForm!: FormGroup
+  changePasswordForm!: FormGroup;
+
+  //  Password rules (same as forgot password)
+  passwordRules = {
+    minLength: false,
+    upper: false,
+    lower: false,
+    digit: false,
+    special: false,
+  };
+
+  showPasswordRules = false;
 
   ngOnInit() {
     this.inItForm();
+    this.listenPasswordChanges();
   }
 
   inItForm() {
     this.changePasswordForm = this.fb.group({
       oldPassword: ['', Validators.required],
       newPassword: ['', Validators.required],
-      confirmPassword: ['', Validators.required]
-    })
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  listenPasswordChanges() {
+    this.changePasswordForm
+      .get('newPassword')!
+      .valueChanges.subscribe((password: string) => {
+        this.checkPasswordRules(password || '');
+      });
+  }
+
+  checkPasswordRules(password: string) {
+    this.passwordRules.minLength = password.length >= 8;
+    this.passwordRules.upper = /[A-Z]/.test(password);
+    this.passwordRules.lower = /[a-z]/.test(password);
+    this.passwordRules.digit = /\d/.test(password);
+    this.passwordRules.special = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  }
+
+  isPasswordValid(): boolean {
+    return Object.values(this.passwordRules).every(Boolean);
+  }
+
+  passwordsMatch(): boolean {
+    const { newPassword, confirmPassword } = this.changePasswordForm.value;
+    return newPassword === confirmPassword;
   }
 
   changePassword() {
     const { newPassword, confirmPassword } = this.changePasswordForm.value;
-    if (this.changePasswordForm.valid && newPassword === confirmPassword) {
-      this.auth_service.updatePassword(this.changePasswordForm.value).subscribe({
-        next: (res: any) => {
-          if (res.status_code === "200") {
-            this.alert_service.success(res.message);
-            this.auth_service.logout();
-          }
-          else this.alert_service.error(res.message);
-        },
-        error: (err) => this.alert_service.error(err)
-      });
+
+    if (this.changePasswordForm.invalid) {
+      this.alert_service.error('Please fill all required fields');
+      return;
     }
-    else {
-      this.alert_service.error('Please Enter Valid Details')
+
+    if (!this.isPasswordValid()) {
+      this.alert_service.error('Password does not meet requirements');
+      return;
     }
+
+    if (newPassword !== confirmPassword) {
+      this.alert_service.error('Passwords do not match');
+      return;
+    }
+
+    this.auth_service.updatePassword(this.changePasswordForm.value).subscribe({
+      next: (res: any) => {
+        if (res.status_code === '200') {
+          this.alert_service.success(res.message);
+          this.auth_service.logout();
+        } else {
+          this.alert_service.error(res.message);
+        }
+      },
+      error: (err) => this.alert_service.error(err),
+    });
   }
 }
